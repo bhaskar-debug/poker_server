7e89fe935fb06e56fbb8f548dac0a9e30b5f3279
Bhaskar <bha6kar@gmail.com>
2024-01-18 11:33:22 +0000

    code updated

--- sample_player/logger.py
+++ sample_player/logger.py
@@ -1,35 +1,36 @@
-import logging
 import os
 from datetime import datetime
+from pathlib import Path
+
 import structlog
 
 
 def get_root_logger(logger_name, filename=None):
     """get the logger object"""
-    logger = logging.getLogger(logger_name)
-    debug = os.environ.get("ENV", "development") == "development"
-    logger.setLevel(logging.DEBUG if debug else logging.INFO)
+    logger = structlog.getLogger()
 
-    formatter = logging.Formatter("%(message)s")
-
-    ch = logging.StreamHandler()
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    if filename:
-        # Add a timestamp to the filename
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename_with_timestamp = f"{filename}_{timestamp}.log"
-        fh = logging.FileHandler(filename_with_timestamp)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
+    # Add a timestamp to the filename
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename_with_timestamp = f"{filename}_{timestamp}"
+    structlog.configure(
+        processors=[
+            # structlog.processors.add_log_level,
+            # structlog.processors.StackInfoRenderer(),
+            # structlog.dev.set_exc_info,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.EventRenamer("msg"),
+            structlog.processors.JSONRenderer(),
+        ],
+        logger_factory=structlog.WriteLoggerFactory(
+            file=Path(filename_with_timestamp).with_suffix(".log").open("wt")
+        ),
+    )
 
     return logger
 
 
-def get_child_logger(root_logger, name):
-    return logging.getLogger(".".join([root_logger, name]))
-
-
 LOG_PATH = "logs"
 logger = get_root_logger(__name__, filename=os.path.join(LOG_PATH, "output"))
+
+if __name__ == "__main__":
+    logger.info("Hi from logger.")
